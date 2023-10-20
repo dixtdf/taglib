eb3e96e66482ac10a676f3d1880fb62240a24b6c
dixtdf <dixtdf@gmail.com>
2023-10-20 16:52:43 +0800

    刷新

--- src/usr/local/emhttp/plugins/taglib/include/setting.php
+++ src/usr/local/emhttp/plugins/taglib/include/setting.php
@@ -1,21 +1,23 @@
 <?PHP
 
-function read_json_file($file)
-{
-    $json = @file_get_contents($file);
-    if (!$json) {
-        $json = [];
-    }
-    return $json;
-}
-
 switch ($_POST['action']) {
     case 'getData':
-        echo json_encode(json_decode(read_json_file("/boot/config/plugins/taglib/taglib.json")));
-        break;
+        $configFilePath = "/boot/config/plugins/taglib/taglib.json";
+        if (!is_file($configFilePath)) {
+            echo '[]';
+            break;
+        }else{
+            $file_contents = file_get_contents($configFilePath);
+            if ($file_contents !== false) {
+                echo $file_contents;
+            } else {
+                echo '[]';
+            }
+            break;
+        }
     case 'applyData':
-        //持久化配置文件
-        file_put_contents("/boot/config/plugins/taglib/taglib.json", json_encode(json_decode($_POST['settings']), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        $configFilePath = "/boot/config/plugins/taglib/taglib.json";
+        file_put_contents($configFilePath, json_encode(json_decode($_POST['settings']), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
         exec("/usr/local/emhttp/plugins/taglib/scripts/createPage.php");
         echo "applyData done";
         break;
