b3a8a1a6d6e9131e27b35ddca906e41b842603bd
dixtdf <dixtdf@gmail.com>
2024-08-10 15:48:35 +0800

    修改构建

--- vue/taglib/vite.config.js
+++ vue/taglib/vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',  // 对应入口文件
+        chunkFileNames: '[name].js',  // 对应代码分块
+        assetFileNames: '[name][extname]',  // 对应静态资源文件
+      }
+    }
+  }
 })
